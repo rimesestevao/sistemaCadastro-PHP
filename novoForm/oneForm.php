9bc6095f71421450484eea353fc9ab3c578ddec6
Estevão Almeida <estevaorimes@gmail.com>
2022-02-15 17:17:35 -0300

    .

--- novoForm/oneForm.php
+++ novoForm/oneForm.php
@@ -85,6 +85,20 @@
         <button type="submit" class="btn btn-dark">Enviar</button>
 
     </form>
+    <?php
+
+        $host = "http://10.0.0.1/phpmyadmin/";
+        $user ="riogamer";
+        $pass = "l1b3r4t0r24";
+        $banco = "riogamer";
+
+        $link = mysqli_connect($host,$user,$pass,$banco);
+        if(mysqli_connect_errno($link)){
+            echo "erro de conexão";
+        }else{
+            echo "conexão estabelecida";
+        }
+    ?>
 </body>
 
 </html>
