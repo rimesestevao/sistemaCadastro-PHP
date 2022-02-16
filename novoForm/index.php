6dce0feda513d71ea883847a6d415d1abefc2e25
Estevão Almeida <estevaorimes@gmail.com>
2022-02-16 14:49:26 -0300

    Update index.php

--- novoForm/index.php
+++ novoForm/index.php
@@ -14,7 +14,17 @@ if (!$conn) {
  
 echo "Connected successfully";
  
-$sql = "INSERT INTO matricula (nome, idade, cpf, rg, sexo, cidade, endereço, email, celular) VALUES (nome, idade, cpf, rg, sexo, cidade, endereço, email, celular)";
+$nome = $_GET["nome"];
+$idade = $_GET["idade"];
+$cpf = $_GET["cpf"];
+$rg = $_GET["rg"];
+$sexo = $_GET["sexo"];
+$cidade = $_GET["cidade"];
+$endereco = $_GET["endereco"];
+$email = $_GET["email"];
+$celular = $_GET["celular"];
+
+$sql = "INSERT INTO matricula (nome, idade, cpf, rg, sexo, cidade, endereco, email, celular) VALUES (nome, idade, cpf, rg, sexo, cidade, endereco, email, celular)";
 
 if (mysqli_query($conn, $sql)) {
       echo "New record created successfully";
