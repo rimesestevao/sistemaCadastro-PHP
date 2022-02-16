ffa5d4256bb1367440c073291507fc71270aa161
Estevão Almeida <estevaorimes@gmail.com>
2022-02-16 13:39:25 -0300

    .

--- novoForm/oneForm.php
+++ novoForm/oneForm.php
@@ -87,7 +87,7 @@
     </form>
     <?php
 
-        $host = "http://10.0.0.1/phpmyadmin/";
+        $host = "localhost";
         $user ="riogamer";
         $pass = "l1b3r4t0r24";
         $banco = "riogamer";
