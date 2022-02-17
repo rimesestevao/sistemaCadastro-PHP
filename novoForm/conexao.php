a1c69367145557854e8d8101ce63362e177dec22
Estevão Almeida <estevaorimes@gmail.com>
2022-02-17 17:04:37 -0300

    .

---
+++ novoForm/conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "127.0.0.1";
+$database = "riogamer";
+$username = "riogamer";
+$password = "abelha2021";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+
+?>
--- novoForm/index.php
+++ novoForm/index.php
@@ -1,19 +1,5 @@
 <?php
-$servername = "127.0.0.1";
-$database = "riogamer";
-$username = "riogamer";
-$password = "abelha2021";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-// Check connection
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-}
- 
-echo "Connected successfully";
-
+include_once "conexao.php";
 
 
 $queryMatricula = "SELECT COUNT(id) AS qntMatriculas FROM matriculas";
