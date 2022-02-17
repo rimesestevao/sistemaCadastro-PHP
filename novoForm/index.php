94a0dba7b182a86d9ea185eca5748e0a81e90f27
Estevão Almeida <estevaorimes@gmail.com>
2022-02-17 17:32:29 -0300

    Update index.php

--- novoForm/index.php
+++ novoForm/index.php
@@ -2,7 +2,7 @@
 
 include "conexao.php";
 
-$sql = "SELECT * FROM users";
+$sql = "SELECT * FROM matricula";
 if ($result=mysqli_query($conn,$sql)) {
     $rowcount=mysqli_num_rows($result);
     echo "The total number of rows are: ".$rowcount; 
