92ca3d7a2a6d15db8fac4a50f99f3a8ab05c0640
Estevão Almeida <estevaorimes@gmail.com>
2022-02-17 17:34:23 -0300

    Update index.php

--- novoForm/index.php
+++ novoForm/index.php
@@ -8,7 +8,7 @@ if ($result=mysqli_query($conn,$sql)) {
     echo "The total number of rows are: ".$rowcount; 
 }
 
-$matricula = $rowcount;
+$matricula = $rowcount + 1;
 $nome = $_GET["nome"];
 $idade = $_GET["idade"];
 $cpf = $_GET["cpf"];
