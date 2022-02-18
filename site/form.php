632c21c3969c723bf9da3a1fefeaf27d30b9ac0d
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-18 16:08:15 -0300

    Update form.php

--- site/form.php
+++ site/form.php
@@ -1,5 +1,5 @@
 <h1>Pesquisar Cursos</h1>
-<form method="POST" action="pesquisar.php">
+<form method="POST" action="testeBusca.php">
     Pesquisar:<input type="text" name="pesquisar" placeholder="PESQUISAR">
     <input type="submit" value="ENVIAR">
 </form>
