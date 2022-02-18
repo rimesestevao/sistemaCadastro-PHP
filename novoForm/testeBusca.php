ede718e325c56c51a404fdc00f974505b2e2201c
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-18 15:40:04 -0300

    Update testeBusca.php

--- novoForm/testeBusca.php
+++ novoForm/testeBusca.php
@@ -1,17 +1,6 @@
-<h1>Pesquisar Matricula</h1>
-<form method="POST" action="pesquisar.php">
-    Pesquisar:<input type="text" name="pesquisar" placeholder="PESQUISAR">
-    <input type="submit" value="ENVIAR">
-</form>
-
 <?php
-   include "conexao.php"
+   include "conexao.php";
 
-    $pesquisar = $_POST['pesquisar'];
-    $result_cursos = "SELECT * FROM matricula WHERE matricula LIKE '%$pesquisar%' LIMIT 5";
-    $resultado_cursos = mysqli_query($conn, $result_cursos);
-
-    while($rows_cursos = mysqli_fetch_array($resultado_cursos)){
-        echo "Numero da matricula: ".$rows_cursos['matricula']."<br>";
-    }
+    $pesquisar = $_POST['search'];
+    echo $pesquisar;
 ?>
