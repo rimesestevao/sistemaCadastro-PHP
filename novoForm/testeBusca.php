032c9dbbc902f9b046ae13814475569d936d4bed
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-18 17:20:31 -0300

    Update testeBusca.php

--- novoForm/testeBusca.php
+++ novoForm/testeBusca.php
@@ -4,7 +4,7 @@
     $pesquisar = $_POST['busca'];
     $result_matricula = "SELECT * from matricula where matricula like '$pesquisar'";
     $result_matricula = mysqli_query ($conn, $result_matricula);
-    echo "$pesquisar";
+    
 
     while($rows_matricula = mysqli($result_matricula)){
         echo "Número da matricula:" .$rows_matricula['matricula']."<br>";
