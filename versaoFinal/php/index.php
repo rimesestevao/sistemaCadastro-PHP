6d84bc5c9195436d090de6a15012277632e50279
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-25 10:47:11 -0300

    Update index.php

--- versaoFinal/php/index.php
+++ versaoFinal/php/index.php
@@ -8,15 +8,15 @@ if ($result=mysqli_query($link,$sql)) {
 }
 
 $matricula = $rowcount + 1;
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-$idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
-$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
-$rg = filter_input(INPUT_POST, 'rg', FILTER_SANITIZE_NUMBER_INT);
-$sexo = filter_input(INPUT_POST, 'sexo', FILTER_SANITIZE_SPECIAL_CHARS);
-$cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_SPECIAL_CHARS);
-$endereco = filter_input(INPUT_POST, 'enereco', FILTER_SANITIZE_SPECIAL_CHARS);
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-$celular = filter_input(INPUT_POST, 'celular', FILTER_SANITIZE_NUMBER_INT);
+$nome = $_GET["nome"];
+$idade = $_GET["idade"];
+$cpf = $_GET["cpf"];
+$rg = $_GET["rg"];
+$sexo = $_GET["sexo"];
+$cidade = $_GET["cidade"];
+$endereco = $_GET["endereco"];
+$email = isset($_GET["email"])?$_GET["email"]:["não informado"];
+$celular = $_GET["celular"];
 
 function validaCPF($cpf) {
       
