9d2b490c1b797a77a835dbfb10a6c470353b6fc6
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-25 17:02:30 -0300

    Update mostrarConsulta.php

--- versaoFinal/php/mostrarConsulta.php
+++ versaoFinal/php/mostrarConsulta.php
@@ -2,16 +2,15 @@
 
 include 'conexao.php';
 
-$pesquisar = $_POST['busca'];
+$pesquisar = $_POST['Buscar'];
 $result_matricula = "SELECT * from matricula where matricula like '$pesquisar'";
 $result_matricula = mysqli_query ($link, $result_matricula);
 
-while($rows_matricula = mysqli_fetch_array($result_matricula)):
+while($rows_matricula = mysqli_fetch_array($result_matricula)){
     echo "Nome:" .$rows_matricula['nome']."<br>";
      
     echo"<tr>";
     echo"<td>$nome</td>";
     echo"<tr>";
-endwhile
-
+}
 ?>
