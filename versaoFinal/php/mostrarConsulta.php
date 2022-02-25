9283909063e8484d098c82e8696cac52cbd47e23
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-25 16:56:13 -0300

    teste correção de bug

--- versaoFinal/php/mostrarConsulta.php
+++ versaoFinal/php/mostrarConsulta.php
@@ -4,18 +4,14 @@ include 'conexao.php';
 
 $querySelect = $link->query("select * from matricula where matricula = '$matricula'");
 
-while ($registro = $querySelect->fetch_assoc()):
-    $matricula = $registro['matricula'];
-    $nome = $registro['nome'];
-    $idade = $registro['idade'];
-    $cpf = $registro['cpf'];
-    $rg = $registro['rg'];
-    $sexo = $registro['sexo'];
-    $cidade = $registro['cidade'];
-    $endereco = $registro['endereco'];
-    $email = $registro['email'];
-    $celular = $_GET['celular'];
 
+$pesquisar = $_POST['busca'];
+$result_matricula = "SELECT * from matricula where matricula like '$pesquisar'";
+$result_matricula = mysqli_query ($link, $result_matricula);
+
+while($rows_matricula = mysqli_fetch_array($result_matricula)):
+    echo "Nome:" .$rows_matricula['nome']."<br>";
+     
     echo"<tr>";
     echo"<td>$nome</td>";
     echo"<tr>";
