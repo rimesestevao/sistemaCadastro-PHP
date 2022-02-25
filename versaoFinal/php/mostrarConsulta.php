b9dde0b2e3a3cdd72af46003240f9da2442f5f1c
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-25 17:05:31 -0300

    desfazendo teste

--- versaoFinal/php/mostrarConsulta.php
+++ versaoFinal/php/mostrarConsulta.php
@@ -2,15 +2,23 @@
 
 include 'conexao.php';
 
-$pesquisar = $_POST['Buscar'];
-$result_matricula = "SELECT * from matricula where matricula like '$pesquisar'";
-$result_matricula = mysqli_query ($link, $result_matricula);
+$querySelect = $link->query("select * from matricula where matricula = '$pesquisar'");
 
-while($rows_matricula = mysqli_fetch_array($result_matricula)){
-    echo "Nome:" .$rows_matricula['nome']."<br>";
-     
+while ($registro = $querySelect->fetch_assoc()):
+    $matricula = $registro['matricula'];
+    $nome = $registro['nome'];
+    $idade = $registro['idade'];
+    $cpf = $registro['cpf'];
+    $rg = $registro['rg'];
+    $sexo = $registro['sexo'];
+    $cidade = $registro['cidade'];
+    $endereco = $registro['endereco'];
+    $email = $registro['email'];
+    $celular = $_GET['celular'];
+
     echo"<tr>";
     echo"<td>$nome</td>";
     echo"<tr>";
-}
+endwhile
+
 ?>
