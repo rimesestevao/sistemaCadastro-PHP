29b97a56ea361ba6e669cba9dffd4c72b34ab0fe
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-25 11:27:01 -0300

    .

--- novoForm/conexao.php
+++ novoForm/conexao.php
@@ -6,14 +6,12 @@ $username = "riogamer";
 $password = "abelha2021";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$link = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
-if (!$conn) {
+if (!$link) {
       die("Connection failed: " . mysqli_connect_error());
 }
- 
 echo "Connected successfully";
-
 
 ?>
