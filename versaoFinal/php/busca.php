e821eca2a7f84a3cd388e316df6fed282e1b779d
Estevão Almeida <estevaorimes@gmail.com>
2022-02-25 23:24:33 -0300

    .

--- versaoFinal/php/busca.php
+++ versaoFinal/php/busca.php
@@ -1,6 +1,3 @@
-<?php session_start(); ?>
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -32,30 +29,29 @@
             
             $pesquisar = $_POST['busca'];
 
-            echo"<br>$pesquisar<br>";
+            if($pesquisar == 0){
+                echo"zerado";
+            }
+            else{
+                $querySelect = $link->query("select * from matricula where matricula = '$pesquisar'");
 
+                while ($registro = $querySelect ->fetch_assoc()):
+                    $matricula = $registro['matricula'];
+                    $nome = $registro['nome'];
+                    $idade = $registro['idade'];
+                    $cpf = $registro['cpf'];
+                    $rg = $registro['rg'];
+                    $sexo = $registro['sexo'];
+                    $cidade = $registro['cidade'];
+                    $endereco = $registro['endereco'];
+                    $email = $registro['email'];
+                    $celular = $_GET['celular'];
 
-            
-            $querySelect = $link->query("select * from matricula where matricula = '$pesquisar'");
-
-            while ($registro = $querySelect ->fetch_assoc()):
-                $matricula = $registro['matricula'];
-                $nome = $registro['nome'];
-                $idade = $registro['idade'];
-                $cpf = $registro['cpf'];
-                $rg = $registro['rg'];
-                $sexo = $registro['sexo'];
-                $cidade = $registro['cidade'];
-                $endereco = $registro['endereco'];
-                $email = $registro['email'];
-                $celular = $_GET['celular'];
-
-                echo"<tr>";
-                echo"<td>$nome</td>";
-                echo"<tr>";
-            endwhile
-
-
+                    echo"<tr>";
+                    echo"<td>$nome</td>";
+                    echo"<tr>";
+                endwhile
+            ;}
         ?>
     </div>
 </body>
