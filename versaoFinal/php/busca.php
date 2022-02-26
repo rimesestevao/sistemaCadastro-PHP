4f740ebcedb3e18b4bd90347bf9afbb2a799efb1
Estevão Almeida <estevaorimes@gmail.com>
2022-02-25 23:31:26 -0300

    Update busca.php

--- versaoFinal/php/busca.php
+++ versaoFinal/php/busca.php
@@ -29,29 +29,30 @@
             
             $pesquisar = $_POST['busca'];
 
-            if($pesquisar = " "){
-                echo"zerado";
-            }
-            else{
-                $querySelect = $link->query("select * from matricula where matricula = '$pesquisar'");
+            echo"<br>$pesquisar<br>";
 
-                while ($registro = $querySelect ->fetch_assoc()):
-                    $matricula = $registro['matricula'];
-                    $nome = $registro['nome'];
-                    $idade = $registro['idade'];
-                    $cpf = $registro['cpf'];
-                    $rg = $registro['rg'];
-                    $sexo = $registro['sexo'];
-                    $cidade = $registro['cidade'];
-                    $endereco = $registro['endereco'];
-                    $email = $registro['email'];
-                    $celular = $_GET['celular'];
 
-                    echo"<tr>";
-                    echo"<td>$nome</td>";
-                    echo"<tr>";
-                endwhile
-            ;}
+            
+            $querySelect = $link->query("select * from matricula where matricula = '$pesquisar'");
+
+            while ($registro = $querySelect->fetch_assoc()):
+                $matricula = $registro['matricula'];
+                $nome = $registro['nome'];
+                $idade = $registro['idade'];
+                $cpf = $registro['cpf'];
+                $rg = $registro['rg'];
+                $sexo = $registro['sexo'];
+                $cidade = $registro['cidade'];
+                $endereco = $registro['endereco'];
+                $email = $registro['email'];
+                $celular = $_GET['celular'];
+
+                echo"<tr>";
+                echo"<td>$nome</td>";
+                echo"<tr>";
+            endwhile
+
+
         ?>
     </div>
 </body>
