9fe50785c4befc2698cf977d80cde17c0cec1964
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-25 22:31:56 -0300

    Update testeBusca.php

--- versaoFinal/php/testeBusca.php
+++ versaoFinal/php/testeBusca.php
@@ -4,9 +4,9 @@
     $pesquisar = $_POST['busca'];
 
     echo"<br>$pesquisar<br>";
-
+    
+    $result_matricula = mysqli_query ($link,$pesquisar);
     $result_matricula = "SELECT * from matricula where matricula like '$pesquisar'";
-    $result_matricula = mysqli_query ($link, $result_matricula);
     
     while($rows_matricula = mysqli_fetch_array($result_matricula)){
         echo "Nome:" .$rows_matricula['nome']."<br>";
