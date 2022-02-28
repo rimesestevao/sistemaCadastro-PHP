299198dad370a7f22257cd61ce54cd326f748eec
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-28 12:35:37 -0300

    Update delete.php

--- versaoFinal/php/delete.php
+++ versaoFinal/php/delete.php
@@ -3,7 +3,7 @@
 include 'conexao.php';
 
 $matricula = filter_input(INPUT_GET, 'matricula', FILTER_SANITIZE_NUMBER_INT);
-$result_usuario = "Delete FROM matricula WHERE nome='$matricula'";
+$result_usuario = "Delete FROM matricula WHERE matricula='$nome'";
 $result_usuario = mysqli_query($link, $result_usuario);
 
 ?>
