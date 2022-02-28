023c5401688dc3662eb4c45e92178d95d6973071
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-02-28 14:41:05 -0300

    teste cookie

--- versaoFinal/php/delete.php
+++ versaoFinal/php/delete.php
@@ -2,7 +2,8 @@
 
 include 'conexao.php';
 
-$matricula = $_REQUEST['pesquisar'];
+//$matricula = $_REQUEST['pesquisar'];
+$matricula= $_COOKIE['matricula'];
 echo "$matricula";
 $result_usuario = "Delete FROM matricula WHERE matricula='$matricula'";
 $result_usuario = mysqli_query($link, $result_usuario);
