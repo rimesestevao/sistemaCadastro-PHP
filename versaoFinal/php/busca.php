dfb301353ec4d4e3a9c73e1fa70855ddceb9052a
Estevão Almeida <estevaorimes@gmail.com>
2022-03-01 12:00:43 -0300

    testes css bootstrap

--- versaoFinal/php/busca.php
+++ versaoFinal/php/busca.php
@@ -27,8 +27,8 @@ $_SESSION["newsession"];
             <button type="submit" class="botaoSolid">Buscar</button>
         </form>
     </div>
-    <div class="cardResultado">
-        <div class="divPHP" >
+    <div class="cardResultado row align-items-center justify-content-between">
+        <div class="divPHP col-6" >
             <?php
                 include 'conexao.php';
                 
@@ -56,7 +56,7 @@ $_SESSION["newsession"];
 
             ?>
         </div>
-        <div class="divForm">
+        <div class="divForm col-6">
             <form  autocomplete="off" method="POST" action="editForm.php">
                 <button class="btnForm" type="submit" value="<?php echo "$pesquisar" ?>" name="edit"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" fill="white" height="24" viewBox="0 0 24 24" width="24"><rect fill="none" height="24" width="24"/><path d="M3,10h11v2H3V10z M3,8h11V6H3V8z M3,16h7v-2H3V16z M18.01,12.87l0.71-0.71c0.39-0.39,1.02-0.39,1.41,0l0.71,0.71 c0.39,0.39,0.39,1.02,0,1.41l-0.71,0.71L18.01,12.87z M17.3,13.58l-5.3,5.3V21h2.12l5.3-5.3L17.3,13.58z"/></svg></button>
             </form>
