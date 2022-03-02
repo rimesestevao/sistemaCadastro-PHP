a6eb973a69f9107e235c3644713f37f6cbd7b6a0
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-03-02 15:59:54 -0300

    teste edit

---
+++ versaoFinal/php/atualizarCadastro.php
@@ -0,0 +1,21 @@
+<?php
+
+$pesquisar = $_POST['edit'];
+
+$matricula = filter_input(INPUT_POST, 'matricula', FILTER_SANITIZE_NUMBER_INT);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_NUMBER_INT);
+$idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+$cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
+$rg = filter_input(INPUT_POST, 'rg', FILTER_SANITIZE_NUMBER_INT);
+$sexo = filter_input(INPUT_POST, 'sexo', FILTER_SANITIZE_NUMBER_INT);
+$cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_NUMBER_INT);
+$endereco = filter_input(INPUT_POST, 'endereco', FILTER_SANITIZE_NUMBER_INT);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_NUMBER_INT);
+$celular = filter_input(INPUT_POST, 'celular', FILTER_SANITIZE_NUMBER_INT);
+
+$queryUpdate = $link->query("update matricula set nome=$nome idade=$idade cpf=$cpf rg=$rg sexo=$sexo cidade=$cidade endereco=$endereco email=$email celular=$celular where matricula = '$pesquisar'");
+$affected_rows = mysqli_affected_rows($link);
+if($affected_rows > 0):
+    header("../busca.php");
+    endif
+?>
