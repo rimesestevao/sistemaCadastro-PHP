b149d29f4a45d961f814ccf1ffeece0b5f9254ec
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-03-02 16:26:08 -0300

    Update atualizarCadastro.php

--- versaoFinal/php/atualizarCadastro.php
+++ versaoFinal/php/atualizarCadastro.php
@@ -4,17 +4,18 @@ include "conexao.php";
 
 $matricula = $_SESSION['matricula'];
 
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_NUMBER_INT);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
 $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
 $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
 $rg = filter_input(INPUT_POST, 'rg', FILTER_SANITIZE_NUMBER_INT);
-$sexo = filter_input(INPUT_POST, 'sexo', FILTER_SANITIZE_NUMBER_INT);
+$sexo = filter_input(INPUT_POST, 'sexo', FILTER_SANITIZE_SPECIAL_CHARS);
 $cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_NUMBER_INT);
-$endereco = filter_input(INPUT_POST, 'endereco', FILTER_SANITIZE_NUMBER_INT);
-$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_NUMBER_INT);
+$endereco = filter_input(INPUT_POST, 'endereco', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $celular = filter_input(INPUT_POST, 'celular', FILTER_SANITIZE_NUMBER_INT);
 
 echo "$cpf";
+echo "$matricula";
 
 $queryUpdate = $link->query("update matricula set nome='$nome' idade='$idade' cpf='$cpf' rg='$rg' sexo='$sexo' cidade='$cidade' endereco='$endereco' email='$email'celular='$celular' where matricula = '$matricula'");
 $affected_rows = mysqli_affected_rows($link);
