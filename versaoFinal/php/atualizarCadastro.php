6eed1c90a83fd59f2ae88fec79652fc34ad686ec
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-03-02 16:22:01 -0300

    Update atualizarCadastro.php

--- versaoFinal/php/atualizarCadastro.php
+++ versaoFinal/php/atualizarCadastro.php
@@ -16,7 +16,7 @@ $celular = filter_input(INPUT_POST, 'celular', FILTER_SANITIZE_NUMBER_INT);
 
 echo "$cpf";
 
-$queryUpdate = $link->query("update matricula set nome=$nome idade=$idade cpf=$cpf rg=$rg sexo=$sexo cidade=$cidade endereco=$endereco email=$email celular=$celular where matricula = '$matricula'");
+$queryUpdate = $link->query("update matricula set nome='$nome' idade='$idade' cpf='$cpf' rg='$rg' sexo='$sexo' cidade='$cidade' endereco='$endereco' email='$email'celular='$celular' where matricula = '$matricula'");
 $affected_rows = mysqli_affected_rows($link);
 if($affected_rows > 0):
     header("../busca.php");
