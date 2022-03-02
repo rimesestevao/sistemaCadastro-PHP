567088f301c74e06e90a880cfdb699bd43bcf3b6
FabricaDeGames <riogamer.fabrica@gmail.com>
2022-03-02 17:55:05 -0300

    Update atualizarCadastro.php

--- versaoFinal/php/atualizarCadastro.php
+++ versaoFinal/php/atualizarCadastro.php
@@ -9,13 +9,14 @@ $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
 $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
 $rg = filter_input(INPUT_POST, 'rg', FILTER_SANITIZE_NUMBER_INT);
 $sexo = filter_input(INPUT_POST, 'sexo', FILTER_SANITIZE_SPECIAL_CHARS);
-$cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_NUMBER_INT);
+$cidade = filter_input(INPUT_POST, 'cidade', FILTER_SANITIZE_SPECIAL_CHARS);
 $endereco = filter_input(INPUT_POST, 'endereco', FILTER_SANITIZE_SPECIAL_CHARS);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $celular = filter_input(INPUT_POST, 'celular', FILTER_SANITIZE_NUMBER_INT);
 
 echo "$cpf<br>";
 echo "$nome<br>";
+echo "$cidade<br>";
 
 $queryUpdate = $link->query("UPDATE matricula set nome='$nome', idade='$idade', cpf='$cpf', rg='$rg', sexo='$sexo', cidade='$cidade', endereco='$endereco', email='$email', celular='$celular' where matricula = '$matricula'");
 $affected_rows = mysqli_affected_rows($link);
