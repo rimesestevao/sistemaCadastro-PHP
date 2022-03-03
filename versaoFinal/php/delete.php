4a2939bcb1ee27b4dfdd2f36f9a0a9c543c968b7
Estevão Almeida <estevaorimes@gmail.com>
2022-03-02 22:45:48 -0300

    Update delete.php

--- versaoFinal/php/delete.php
+++ versaoFinal/php/delete.php
@@ -6,6 +6,10 @@ $matricula = $_POST['delete'];
 echo "$matricula";
 $result_usuario = "Delete FROM matricula WHERE matricula='$matricula'";
 $result_usuario = mysqli_query($link, $result_usuario);
+ 
+echo '<script type ="text/JavaScript">';  
+echo 'alert("JavaScript Alert Box by PHP")';  
+echo '</script>';  
 
 header('Location: busca.php');
 
