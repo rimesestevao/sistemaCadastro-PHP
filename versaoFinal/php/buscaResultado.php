6ac90d31666b8a334bf4a3cea1ad27744f6f0aed
Estevão Almeida <estevaorimes@gmail.com>
2022-03-10 12:13:31 -0300

    Update buscaResultado.php

--- versaoFinal/php/buscaResultado.php
+++ versaoFinal/php/buscaResultado.php
@@ -27,7 +27,7 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
-    <script src="../scripts/main.js" defer></script>
+    <script src="../scripts/buscaResultado.js" defer></script>
 </head>
 
 <body>
