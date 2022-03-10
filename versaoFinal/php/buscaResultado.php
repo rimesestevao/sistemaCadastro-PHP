ba715ed750579b16abdb624d2cd50640209007d9
Estevão Almeida <estevaorimes@gmail.com>
2022-03-10 11:16:29 -0300

    Update buscaResultado.php

--- versaoFinal/php/buscaResultado.php
+++ versaoFinal/php/buscaResultado.php
@@ -49,7 +49,8 @@
             </div>
             <div class="box3">
                 <form id="formDelete"  autocomplete="off" method="POST" action="delete.php">
-                    <button class="btnForm" onClick="alertDelete('Sucesso!','Você apagou o cadastro.');" value="<?php echo "$pesquisar" ?>" name="delete"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" fill="white" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg></button>
+                    <input hidden value="<?php echo "$pesquisar" ?>" name="delete" />
+                    <button class="btnForm" onClick="alertDelete('Sucesso!','Você apagou o cadastro.');" ><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" fill="white" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg></button>
                 </form>
             </div>
         </div>
