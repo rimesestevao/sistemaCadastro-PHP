8368c89532842ed40bf77d40dcc3087a6838b3dc
Estevão Almeida <estevaorimes@gmail.com>
2022-03-11 22:35:16 -0300

    .

---
+++ form-react/src/components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='form'>
+        <div class="form__container">
+            <h2>Cadastro com React.JS</h2>
+            <div class="form__containerBox">
+                <div class="form__containerBoxInput">
+                    <label></label>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Form
